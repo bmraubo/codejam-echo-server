1875239e8e104fa019a7646a0009ce5f10d6815c
Bartosz Raubo <braubo@8thlight.com>
2022-07-20 21:04:04 +0100

    client

--- src/client.py
+++ src/client.py
@@ -1,6 +1,7 @@
 import websockets
 from websockets.client import WebSocketClientProtocol
 import typing
+import asyncio
 
 URI = "localhost:4444"
 
@@ -15,3 +16,14 @@ async def send_message(connection: WebSocketClientProtocol, message: str):
 
 async def receive_message(connection: WebSocketClientProtocol) -> str:
     await connection.recv()
+
+async def main():
+    while True:
+        connection = await start_client()
+        message = await get_input()
+        await send_message(connection, message)
+        received = await receive_message(connection)
+        print(received)
+
+if __name__ == "__main__":
+    asyncio.run(main())
