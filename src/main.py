805203a16113d05852d46596b262a0561e766f81
Bartosz Raubo <braubo@8thlight.com>
2022-07-20 22:47:34 +0100

    starlette implementation

--- src/main.py
+++ src/main.py
@@ -1,23 +1,21 @@
 import sys
 import asyncio
-from fastapi import FastAPI, WebSocket
 import uvicorn
-
+from starlette.websockets import WebSocket
+from starlette.routing import WebSocketRoute
+from starlette.applications import Starlette
 
 HOST = "localhost"
 PORT = 4444
 
-app = FastAPI()
-
-@app.websocket("/")
-async def websocket_endpoint(websocket: WebSocket):
+async def app(scope, receive, send):
+    websocket = WebSocket(scope=scope, receive=receive, send=send)
     await websocket.accept()
-    while True:
-        message = await websocket.receive_text()
-        await websocket.send_text(message)
-        if message == "close":
-            websocket.close()
-            sys.exit()
+    message: str = await websocket.receive_text()
+    await websocket.send_text(message)
+    if message == "close":
+        await websocket.close()
+        sys.exit()
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=4444, debug=True)
